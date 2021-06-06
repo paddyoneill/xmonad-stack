99e20312c9dbac6f40be423d952619881960295f
Patrick O'Neill <paddy.oneill93@gmail.com>
2021-06-06 10:41:53 +0100

    Update terminal and modMask settings
    Set default terminal emulator to alacritty
    Set modMast to Super key

--- xmonad.hs
+++ xmonad.hs
@@ -1,3 +1,39 @@
 import XMonad
 
+--------------------------------------------------------------------------------
+-- General settings
+--------------------------------------------------------------------------------
+
+{-
+modMask sets which key is used as the modifier key
+Possible values are
+mod1Mask (LAlt)
+mod2Mask (RAlt)
+mod4Mask (Super / Windows)
+
+Default: mod1Mask
+-}
+
+modMask' :: KeyMask
+modMask' = mod4Mask
+
+{-
+Main terminal emulator
+
+Default: xterm
+-}
+
+terminal' :: String
+terminal' = "alacritty"
+
+
+--------------------------------------------------------------------------------
+-- Main function
+--------------------------------------------------------------------------------
+
+main:: IO ()
 main = xmonad def
+  {
+    terminal = terminal'
+  , modMask = modMask'
+  }
